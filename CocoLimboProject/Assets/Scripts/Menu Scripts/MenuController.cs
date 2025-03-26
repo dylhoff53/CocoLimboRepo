2c74f7b863b18beb19ef7d8ef5b99c370100baab
Race Williams <willr108@my.erau.edu>
2025-03-25 22:29:40 -0700

    Settings Page

--- CocoLimboProject/Assets/Scripts/Menu Scripts/MenuController.cs
+++ CocoLimboProject/Assets/Scripts/Menu Scripts/MenuController.cs
@@ -2,8 +2,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
+    public GameObject settingsMenu;
+    public GameObject buttons;
+
     public void StartGame()
     {
         SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OpenSettings()
+    {
+        settingsMenu.SetActive(true);
+        buttons.SetActive(false);
+    }
+
+    public void CloseSettings()
+    {
+        settingsMenu?.SetActive(false);
+        buttons?.SetActive(true);
     }
 }
